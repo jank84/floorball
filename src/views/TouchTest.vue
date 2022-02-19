405c86a4d84cc32381786351adf42072106df25f
Jan Kostulski <gitlab@jankostulski.de>
2022-02-19 11:59:51 +0100

    update formatting

--- src/views/TouchTest.vue
+++ src/views/TouchTest.vue
@@ -2,13 +2,19 @@
   <div class="about">
     <!-- https://codepen.io/sirlancelot/pen/NNGYEP -->
     <svg class="graph" v-bind="graphPos">
-      <circle class="pointer" v-bind="circlePos" @mousedown="startMove" @touchstart.prevent="startMove"></circle>
+      <circle
+        class="pointer"
+        v-bind="circlePos"
+        @mousedown="startMove"
+        @touchstart.prevent="startMove"
+      ></circle>
     </svg>
   </div>
 </template>
 
 
-<script>
+
+<script lang="js">
 export default {
   data() {
     return {
@@ -16,77 +22,78 @@ export default {
       circlePos: {
         cx: 0,
         cy: 0,
-        r: 8
-		},
-		graphSize: 100
-    }
+        r: 8,
+      },
+      graphSize: 100,
+    };
   },
   mounted() {
-    console.log(`The initial count is ${this.count}.`)
+    console.log(`The initial count is ${this.count}.`);
   },
-	computed: {
-		graphPos() {
-			var size = this.graphSize
-			var half = size / 2
-			return {
-				viewBox: [-half, -half, size, size].join(" "),
-				width: size
-			}
-		}
-	},
-	methods: {
-		startMove(evt) {
-			const touch = (evt.type === "touchstart")
-			if (!touch && evt.button !== 0) return;
-			const events = touch ? {
-				move: "touchmove",
-				stop: "touchend"
-			} : {
-				move: "mousemove",
-				stop: "mouseup"
-			}
-			const elem = evt.currentTarget.closest("svg")
-			const point = elem.createSVGPoint()
-			const transform = elem.getScreenCTM().inverse()
-			const circlePos = this.circlePos
-			const getPos = touch ? getTouchPos : getMousePos
+  computed: {
+    graphPos() {
+      var size = this.graphSize;
+      var half = size / 2;
+      return {
+        viewBox: [-half, -half, size, size].join(" "),
+        width: size,
+      };
+    },
+  },
+  methods: {
+    startMove(evt) {
+      const touch = evt.type === "touchstart";
+      if (!touch && evt.button !== 0) return;
+      const events = touch
+        ? {
+            move: "touchmove",
+            stop: "touchend",
+          }
+        : {
+            move: "mousemove",
+            stop: "mouseup",
+          };
+      const elem = evt.currentTarget.closest("svg");
+      const point = elem.createSVGPoint();
+      const transform = elem.getScreenCTM().inverse();
+      const circlePos = this.circlePos;
+      const getPos = touch ? getTouchPos : getMousePos;
 
-			var moving = true
-			var newPt
+      var moving = true;
+      var newPt;
 
-			const updateFn = () => {
-				if (moving) requestAnimationFrame(updateFn)
+      const updateFn = () => {
+        if (moving) requestAnimationFrame(updateFn);
 
-				// Map the screen pixels back to svg coords
-				newPt = point.matrixTransform(transform)
-				circlePos.cx = newPt.x
-				circlePos.cy = newPt.y
-			}
-			const moveFn = (evt) => getPos(evt, point)
-			const stopFn = (evt) => {
-				moving = false
-				elem.removeEventListener(events.move, moveFn)
-				elem.removeEventListener(events.stop, stopFn)
-			}
+        // Map the screen pixels back to svg coords
+        newPt = point.matrixTransform(transform);
+        circlePos.cx = newPt.x;
+        circlePos.cy = newPt.y;
+      };
+      const moveFn = (evt) => getPos(evt, point);
+      const stopFn = (evt) => {
+        moving = false;
+        elem.removeEventListener(events.move, moveFn);
+        elem.removeEventListener(events.stop, stopFn);
+      };
 
-			requestAnimationFrame(updateFn)
-			moveFn(evt)
+      requestAnimationFrame(updateFn);
+      moveFn(evt);
 
-			elem.addEventListener(events.move, moveFn)
-			elem.addEventListener(events.stop, stopFn)
-		}
-	}
-}
-
+      elem.addEventListener(events.move, moveFn);
+      elem.addEventListener(events.stop, stopFn);
+    },
+  },
+};
 
 function getMousePos(mouseEvent, point) {
-	point.x = (mouseEvent.clientX)
-	point.y = (mouseEvent.clientY)
+  point.x = mouseEvent.clientX;
+  point.y = mouseEvent.clientY;
 }
 
 function getTouchPos(touchEvent, point) {
-	point.x = (touchEvent.touches[0].clientX)
-	point.y = (touchEvent.touches[0].clientY)
+  point.x = touchEvent.touches[0].clientX;
+  point.y = touchEvent.touches[0].clientY;
 }
 </script>
 
@@ -113,81 +120,24 @@ function getTouchPos(touchEvent, point) {
   fill: #42b983;
   cursor: pointer;
 }
-
 </style>
 
-
-
-new Vue({
-	el: "#app",
-	data: {
-		circlePos: {
-			cx: 0,
-			cy: 0,
-			r: 8
-		},
-		graphSize: 100
-	},
-	computed: {
-		graphPos() {
-			var size = this.graphSize
-			var half = size / 2
-			return {
-				viewBox: [-half, -half, size, size].join(" "),
-				width: size
-			}
-		}
-	},
-	methods: {
-		startMove(evt) {
-			const touch = (evt.type === "touchstart")
-			if (!touch && evt.button !== 0) return;
-			const events = touch ? {
-				move: "touchmove",
-				stop: "touchend"
-			} : {
-				move: "mousemove",
-				stop: "mouseup"
-			}
-			const elem = evt.currentTarget.closest("svg")
-			const point = elem.createSVGPoint()
-			const transform = elem.getScreenCTM().inverse()
-			const circlePos = this.circlePos
-			const getPos = touch ? getTouchPos : getMousePos
-
-			var moving = true
-			var newPt
-
-			const updateFn = () => {
-				if (moving) requestAnimationFrame(updateFn)
-
-				// Map the screen pixels back to svg coords
-				newPt = point.matrixTransform(transform)
-				circlePos.cx = newPt.x
-				circlePos.cy = newPt.y
-			}
-			const moveFn = (evt) => getPos(evt, point)
-			const stopFn = (evt) => {
-				moving = false
-				elem.removeEventListener(events.move, moveFn)
-				elem.removeEventListener(events.stop, stopFn)
-			}
-
-			requestAnimationFrame(updateFn)
-			moveFn(evt)
-
-			elem.addEventListener(events.move, moveFn)
-			elem.addEventListener(events.stop, stopFn)
-		}
-	}
-})
-
-function getMousePos(mouseEvent, point) {
-	point.x = (mouseEvent.clientX)
-	point.y = (mouseEvent.clientY)
-}
-
-function getTouchPos(touchEvent, point) {
-	point.x = (touchEvent.touches[0].clientX)
-	point.y = (touchEvent.touches[0].clientY)
-}
+new Vue({ el: "#app", data: { circlePos: { cx: 0, cy: 0, r: 8 }, graphSize: 100
+}, computed: { graphPos() { var size = this.graphSize var half = size / 2 return
+{ viewBox: [-half, -half, size, size].join(" "), width: size } } }, methods: {
+startMove(evt) { const touch = (evt.type === "touchstart") if (!touch &&
+evt.button !== 0) return; const events = touch ? { move: "touchmove", stop:
+"touchend" } : { move: "mousemove", stop: "mouseup" } const elem =
+evt.currentTarget.closest("svg") const point = elem.createSVGPoint() const
+transform = elem.getScreenCTM().inverse() const circlePos = this.circlePos const
+getPos = touch ? getTouchPos : getMousePos var moving = true var newPt const
+updateFn = () => { if (moving) requestAnimationFrame(updateFn) // Map the screen
+pixels back to svg coords newPt = point.matrixTransform(transform) circlePos.cx
+= newPt.x circlePos.cy = newPt.y } const moveFn = (evt) => getPos(evt, point)
+const stopFn = (evt) => { moving = false elem.removeEventListener(events.move,
+moveFn) elem.removeEventListener(events.stop, stopFn) }
+requestAnimationFrame(updateFn) moveFn(evt) elem.addEventListener(events.move,
+moveFn) elem.addEventListener(events.stop, stopFn) } } }) function
+getMousePos(mouseEvent, point) { point.x = (mouseEvent.clientX) point.y =
+(mouseEvent.clientY) } function getTouchPos(touchEvent, point) { point.x =
+(touchEvent.touches[0].clientX) point.y = (touchEvent.touches[0].clientY) }
