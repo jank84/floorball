ee57c7d16dd1f69074f091ca5f7c42fad88dc967
Jan K <jan.kostulski@web.de>
2022-02-21 13:55:59 +0100

    update cmts

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-import { VuesticPlugin } from "vuestic-ui"; // <-
-import "vuestic-ui/dist/vuestic-ui.css"; // <-
+import { VuesticPlugin } from "vuestic-ui"; // material ui
+import "vuestic-ui/dist/vuestic-ui.css"; // material ui
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(VuesticPlugin); // <-
+app.use(VuesticPlugin); // material ui
 
 app.mount("#app");
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <!-- <h1>This is an about page</h1>
+    <br /> -->
+    <va-button outline @click="$vaToast.init('Test notification')">Test notification</va-button>
+    <va-button outline @click="btn_function1()">Inc</va-button>
     <br />
-    <va-card>
-      <va-card-title>Title </va-card-title>
-      <va-card-content
-        >Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit.</va-card-content
-      >
-    </va-card>
+    <br />
+    <va-badge right overlap :text="store.counter >= 0 ? store.counter : ''">
+      <va-button @click="btn_function1" icon="add"> Increate Example counter </va-button>
+    </va-badge>
+    <br />
+    <br />
+    <!-- todo: check why line breaks br doe not work. HMR issue -->
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates delectus, a laudantium quam necessitatibus cupiditate omnis animi maiores aperiam magni aspernatur sapiente earum sint eaque sunt dolor laboriosam? Amet, accusantium.</p>
     <br />
     <va-card>
       <va-image src="https://picsum.photos/400/200" style="height: 200px" />
@@ -20,6 +24,28 @@
   </div>
 </template>
 
+<script lang="ts">
+import { useCounterStore } from "@/stores/counter.ts";
+
+export default {
+  setup() {
+    const store = useCounterStore()
+    const btn_function1 = () => {
+      store.increment()
+      // alert("foo")
+    }
+  // const toast = (config: any) => {
+  //     if (app) {
+  //       const vaToast = app.appContext.config.globalProperties.$vaToast
+  //       vaToast.init(config)
+  //     }
+    // }
+
+    return { store, btn_function1 }
+  },
+}
+</script>
+
 <style>
 @media (min-width: 1024px) {
   .about {
@@ -27,5 +53,10 @@
     display: flex;
     align-items: center;
   }
+}
+.about {
+  display: block;
+  position: relative;
+  top: 10%;
 }
 </style>
