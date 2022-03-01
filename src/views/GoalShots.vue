e1e3d25f284d6d1b3405ae3c6b2847d8854c9064
Jan K <jan.kostulski@web.de>
2022-03-01 18:06:37 +0100

    revert old svg viewBox

--- src/views/GoalShots.vue
+++ src/views/GoalShots.vue
@@ -3,7 +3,7 @@
       DEBUG TEXT: {{debug_text}}
 
     </div>
-    <svg width="100%" height="100%" viewBox="0 0 900 383" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;"
+    <svg width="100%" height="100%" viewBox="0 0 737 383" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;"
       @mousedown="startMove"
       @touchstart.prevent="startMove"
       @mouseup="stopMove"
