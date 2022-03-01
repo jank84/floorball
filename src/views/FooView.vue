c27b6199f30f1b5a6202a46ab1ca9708f36f8414
Jan K <jan.kostulski@web.de>
2022-03-01 18:22:15 +0100

    add test FooView

--- src/views/FooView.vue
+++ src/views/FooView.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>
-    foo
-  </div>
+    {{raw_json}}
 </template>
 
 <script setup lang="ts">
-
+import { ref } from "vue"
+import { use_goal_shot_store } from "@/stores/goal_shots";
+  const store = use_goal_shot_store()
+  const raw_json = ref(JSON.stringify(
+    store.$state.last_goal_shot_data,
+    null,
+    2
+  ));
 </script>
