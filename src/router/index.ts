b83a2a0e92a0298d766caa8f8fd5b7cd1392fb87
Jan K <jan.kostulski@web.de>
2022-03-01 18:09:12 +0100

    update router: nested routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,34 +1,55 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import MainContent from "../views/MainContent.vue";
 import CounterTest from "../views/CounterTest.vue";
 import GoalShots from "../views/GoalShots.vue";
+import FooView from "../views/FooView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "about",
+      name: "main",
+      component: MainContent,
+      children: [
+        {
+          path: "/counter-test",
+          name: "counter-test",
+          component: CounterTest,
+        },
+        {
+          path: "/home",
+          name: "home",
+          component: HomeView,
+        },
+        {
+          path: "/goal-shots",
+          name: "goal-shots",
+          component:GoalShots,
+        },
+      ]
+    },
+    // ⬇ without nav ⬇
+    {
+      path: "/blank/counter-test",
+      name: "blank-counter-test",
       component: CounterTest,
     },
     {
-      path: "/counter-test",
-      name: "counter-test",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: CounterTest,
-    },
-    {
-      path: "/home",
-      name: "home",
+      path: "/blank/home",
+      name: "blank-home",
       component: HomeView,
     },
     {
-      path: "/goal-shots",
-      name: "goal-shots",
-      // @ts-ignore // ignore for now, since its not valid typescript but js
+      path: "/blank/goal-shots",
+      name: "blank-goal-shots",
       component:GoalShots,
+    },
+    {
+      path: "/fooview",
+      name: "fooview",
+      component:FooView,
     },
   ],
 });
